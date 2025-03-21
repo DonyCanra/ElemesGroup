12cec884981b986692a4e7e2d0a11264de798cf6
DonyAssistId <125004719+DonyCanra@users.noreply.github.com>
2025-03-22 02:15:43 +0700

    [FIX]: Fixing Render Images

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -6,7 +6,7 @@ export const categories = [
   { id: 5, name: "Doughnut", stock: 11, icons: "/assets/Icons/doughnut.svg", backgroundColor: "#F3F7D9" },
   { id: 6, name: "Cupcake", stock: 22, icons: "/assets/Icons/cupcake.svg", backgroundColor: "#F0FEEB" },
   { id: 7, name: "Pizza", stock: 25, icons: "/assets/Icons/pizza.svg", backgroundColor: "#E6F3F5" },
-  { id: 8, name: "Kebab", stock: 12, icons: "/assets/Icons/Kebab.svg", backgroundColor: "#EAEEFA" },
+  { id: 8, name: "Kebab", stock: 12, icons: "/assets/Icons/kebab.svg", backgroundColor: "#EAEEFA" },
   { id: 9, name: "Salmon", stock: 22, icons: "/assets/Icons/salmon.svg", backgroundColor: "#F9EEF3" },
   { id: 10, name: "Doughnut", stock: 11, icons: "/assets/Icons/doughnut.svg", backgroundColor: "#F3F7D9" },
 ];
